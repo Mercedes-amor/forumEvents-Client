6ee291b04ced12f09831367d33006f4a0a05b0a1
Lucas Navarro <navarrorlucas@gmail.com>
2023-09-07 17:33:26 +0200

    done

--- src/components/FilteredEventsFooterSearch.jsx
+++ src/components/FilteredEventsFooterSearch.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
+import ciencia from  "../assets/ciencia.png"
+import pizza from "../assets/pizza.png"
+import  tec from "../assets/tec.png"
+import medicina from "../assets/medicina.png"
+import otros from "../assets/otros.png"
+import entretenimiento from "../assets/entretenimiento.png"
 
 export default function FilteredEventsFooterSearch() {
 
@@ -16,12 +22,12 @@ export default function FilteredEventsFooterSearch() {
 
         <h3>Todos los sectores</h3>
     
-    <Link to="/events/tecnológico" ><Button><img src="public\techno-heart.png" alt="logo" width={35}/>tecnológico</Button></Link>
-    <Link to="/events/medicina" ><Button><img src="public\doctor-20-regular.png" alt="logo" width={35}/> medicina</Button></Link>
-    <Link to="/events/ciencia" ><Button><img src="public\materials-science.png" alt="logo" width={35}/> ciencia</Button></Link>
-    <Link to="/events/gastronómico" ><Button><img src="public\food-pizza-20-regular.png" alt="logo" width={35}/> gastronómico</Button></Link>
-    <Link to="/events/ocio" ><Button><img src="public\entertainment-party-popper-hobby-entertainment-party-popper-confetti-event.png" alt="logo" width={35}/> ocio</Button></Link>
-    <Link to="/events/Otro" ><Button><img src="public\other-admission-outline.png" alt="logo" width={35}/> Otro</Button></Link>
+    <Link to="/events/tecnológico" ><Button><img src={tec} alt="logo" width={35}/>tecnológico</Button></Link>
+    <Link to="/events/medicina" ><Button><img src={medicina} alt="logo" width={35}/> medicina</Button></Link>
+    <Link to="/events/ciencia" ><Button><img src={ciencia} alt="logo" width={35}/> ciencia</Button></Link>
+    <Link to="/events/gastronómico" ><Button><img src={pizza} alt="logo" width={35}/> gastronómico</Button></Link>
+    <Link to="/events/ocio" ><Button><img src={entretenimiento} alt="logo" width={35}/> ocio</Button></Link>
+    <Link to="/events/Otro" ><Button><img src={otros} alt="logo" width={35}/> Otro</Button></Link>
   
    
   </div>
